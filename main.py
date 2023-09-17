8b9ee3f2b22f41333d9ef912dc33ebdf62b506d1
oDqnger <103481200+oDqnger@users.noreply.github.com>
2023-09-17 11:04:56 +0100

    Added a feature

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import keyboard
 import time
 
 def change_settings():
-    exit_hotkey, delay, how_long = "", "", ""
+    exit_hotkey, delay, how_long, amount_of_times = "", "", "", ""
     message = input("What's the message you would like to spam? ")
     while True:
         try:
@@ -21,11 +21,19 @@ def change_settings():
                     continue
 
             except:
-                if how_long == "":
-                    break
-                else:
+                if how_long != "" or how_long == 0:
                     print("ERROR! Please type in a number (seconds)")
                     continue
+
+
+            amount_of_times = int(input("OPTIONAL (to skip this press enter): How many times do you want the program to spam the message for: "))
+
+            if amount_of_times <= 0:
+                print("ERROR! The number must be greater than 0, not less than or equal to 0.")
+                continue
+            elif type(amount_of_times) == type("s") and amount_of_times != "":
+                print("ERROR! Please enter a number!")
+                continue
 
         except:
             print("ERROR! Please enter a number!")
@@ -33,12 +41,12 @@ def change_settings():
 
         break
 
-    return [message, delay, exit_hotkey, how_long]
+    return [message, delay, exit_hotkey, how_long, amount_of_times]
 
 def main():
 
-    message, delay, exit_hotkey, how_long = change_settings()
-
+    message, delay, exit_hotkey, how_long, amount_of_times = change_settings()
+    print(amount_of_times)
     while True:
 
         print("The spammer will begin in 5 seconds...")
@@ -46,16 +54,30 @@ def main():
         print("Spammer started!")
 
         start, end = time.time(), 0
-        while True if how_long == "" else end - start <= how_long:
-            if keyboard.is_pressed(exit_hotkey):
-                print("Exiting!")
-                break
 
-            keyboard.write(message)
-            keyboard.press("enter")
+        if amount_of_times == "":
 
-            time.sleep(delay)
-            end = time.time()
+            while True if how_long == "" else end - start <= how_long:
+                if keyboard.is_pressed(exit_hotkey):
+                    print("Exiting!")
+                    break
+
+                keyboard.write(message)
+                keyboard.press("enter")
+
+                time.sleep(delay)
+                end = time.time()
+        else:
+            for x in range(0, amount_of_times):
+                if keyboard.is_pressed(exit_hotkey):
+                    print("Exiting!")
+                    break
+
+                keyboard.write(message)
+                keyboard.press("enter")
+
+                time.sleep(delay)
+                end = time.time()
 
         print("Spammer ended.\n")
 
@@ -63,7 +85,7 @@ def main():
             prompt = input("Would you like to change the settings, would you like to resume or would you want to quit the spammer? (c/r/q): ").lower()
 
             if prompt == "c":
-                message, delay, exit_hotkey, how_long = change_settings()
+                message, delay, exit_hotkey, how_long, amount_of_times = change_settings()
                 break
             elif prompt == "r":
                 break
